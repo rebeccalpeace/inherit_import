8c206b70a268939bd4ef72059644253f4d5ee9ed
rebeccalpeace <rebeccalpeace@gmail.com>
2022-07-26 19:13:27 -0400

    first commit

---
+++ geo_module.py
@@ -0,0 +1,12 @@
+import math
+
+def circle_area(radius):
+   area = math.pi * (radius**2)
+   return area
+
+
+def find_hypot(side1, side2):
+    hypot = math.sqrt(side1**2 + side2**2)
+    return hypot
+
+
